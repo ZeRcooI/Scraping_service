9c4f33a8440d574e483af66786709c0b7522fe41
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-03-05 23:49:52 +0300

    внесены все изменения касаемо формы запроса пользователя

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+
+from scraping.forms import FindForm
 from scraping.models import Vacancy
 
 
 def home_view(request):
-    queryset = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': queryset})
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    queryset = []
+
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
+        queryset = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': queryset, 'form': form})
