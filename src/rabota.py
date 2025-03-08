bcc108e06c543e4ba5a187aba77b58f6098518c7
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-03-08 16:14:56 +0300

    добавлен ещё один сайт для скрапинга

--- src/rabota.py
+++ src/rabota.py
@@ -14,7 +14,7 @@ def rabota(url):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         soup = BS(response.content, 'html.parser')
-        main_div = soup.find('div', class_='home-vacancies__infinity-list')
+        main_div = soup.find('div', class_='infinity-scroll')
         if main_div:
             div_list = main_div.find_all('div', attrs={'class': 'vacancy-preview-card__top'})
             for div in div_list:
@@ -46,7 +46,7 @@ def rabota(url):
     return jobs, errors
 
 
-def superjob(url):
+def gorodrabot(url):
     jobs = []
     errors = []
     domain = 'https://moskva.gorodrabot.ru/'
@@ -61,8 +61,6 @@ def superjob(url):
                 href = title.a['href']
                 content = div.find('div', attrs={'class': 'snippet__desc'}).text
                 company = div.find('span', attrs={'class': 'snippet__meta-value'}).text
-
-                a= 1
 
                 jobs.append({
                     'title': title.text,
@@ -84,8 +82,48 @@ def superjob(url):
     return jobs, errors
 
 
+def superjob(url):
+    jobs = []
+    errors = []
+    domain = 'https://russia.superjob.ru/'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BS(response.content, 'html.parser')
+        main_div = soup.find('div', class_='MokF1')
+        if main_div:
+            div_list = main_div.find_all('div', class_ = 'qjjga')
+            for div in div_list:
+                title = div.find('div', attrs={'class': 'T8ZHO'})
+                href = title.a['href']
+
+                content_tag = div.find('span', class_='mOvi3 _2BmV8 OAzFF J1J_3 yqr63')
+                content = content_tag.text.strip() if content_tag else "Нет описания"
+
+                company_tag = div.find('span', class_='f-test-text-vacancy-item-company-name')
+                company_name = company_tag.a.text.strip() if company_tag and company_tag.a else "Нет компании"
+
+                jobs.append({
+                    'title': title.text,
+                    'url': domain + href,
+                    'description': content,
+                    'company': company_name,
+                })
+        else:
+            errors.append({
+                'url': url,
+                'title': 'Div does not exist',
+            })
+    else:
+        errors.append({
+            'url': url,
+            'title': 'Page do not response',
+        })
+
+    return jobs, errors
+
+
 if __name__ == '__main__':
-    url = 'https://moskva.gorodrabot.ru/'
+    url = 'https://russia.superjob.ru/vacancy/search/?keywords=Python'
     jobs, errors = superjob(url)
     h = codecs.open('rabota.json', 'w', 'utf-8')
     h.write(str(jobs))
