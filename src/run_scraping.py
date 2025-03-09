a87e9c2abd0856d2e04a283f0fd37ef1e2f852e6
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-03-09 22:34:27 +0300

    допилен функционал по запуску асинхронной функции run_scraping.py

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,7 +1,11 @@
+import asyncio
 import codecs
 import os, sys
-from django.db import DatabaseError
 
+from django.contrib.auth import get_user_model
+from django.db import DatabaseError
+from django.template.defaulttags import querystring
+from django.templatetags.i18n import language
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -11,28 +15,67 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 import django
 django.setup()
 from scraping.parsers import *
-from scraping.models import Vacancy, City, Language, Error
+from scraping.models import Vacancy, City, Language, Error, Url
+User = get_user_model()
 
 
 parsers = (
-    (rabota, 'https://www.rabota.ru/vacancy/?query=Developer%20python'),
-    (gorodrabot, 'https://moskva.gorodrabot.ru/python'),
-    (superjob, 'https://www.superjob.ru/vacancy/search/?keywords=Python&without_resume_send_on_vacancy=1&geo%5Bt%5D%5B0%5D=4&click_from=facet'),
+    (rabota, 'rabota'),
+    (gorodrabot, 'gorodrabot'),
+    (superjob, 'superjob'),
 )
 
 
-city = City.objects.filter(slug='moskva').first()
-language = Language.objects.filter(slug='python').first()
-
-
 jobs, errors = [], []
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
+def get_settings():
+    queryset = User.objects.filter(send_email=True).values()
+    settings_list = set((q['city_id'], q['language_id']) for q in queryset)
+    return settings_list
 
 
+def get_urls(_settings):
+    queryset = Url.objects.all().values()
+    url_dct = {
+        (q['city_id'], q['language_id']): q['url_data'] for q in queryset
+    }
+    urls = []
+    for pair in _settings:
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['language'] = pair[1]
+        tmp['url_data'] = url_dct[pair]
+        urls.append(tmp)
+    return urls
+
+
+async def main(value):
+    func, url, city, language = value
+    job, error  = await loop.run_in_executor(None, func, url, city, language)
+    errors.extend(error)
+    jobs.extend(job)
+
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+
+loop = asyncio.get_event_loop()
+tmp_task = [(func, data['url_data'][key], data['city'], data['language'])
+             for data in url_list
+             for func, key in parsers]
+tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_task])
+
+# for data in url_list:
+#     for func, key in parsers:
+#         url = data['url_data'][key]
+#         j, e = func(url, city=data['city'], language=data['language'])
+#         jobs += j
+
+
+loop.run_until_complete(tasks)
+loop.close()
 for job in jobs:
-    v = Vacancy(**job, city=city, language=language)
+    v = Vacancy(**job)
     try:
         v.save()
     except DatabaseError:
--- src/scraping/parsers.py
+++ src/scraping/parsers.py
@@ -19,119 +19,128 @@ headers = [
 ]
 
 
-def rabota(url):
+def rabota(url, city=None, language=None):
     jobs = []
     errors = []
     domain = 'https://www.rabota.ru/'
-    response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if response.status_code == 200:
-        soup = Bs(response.content, 'html.parser')
-        main_div = soup.find('div', class_='infinity-scroll')
-        if main_div:
-            div_list = main_div.find_all('div', attrs={'class': 'vacancy-preview-card__top'})
-            for div in div_list:
-                title = div.find('h3')
-                href = title.a['href']
-                content = div.find('div', attrs={'class': 'vacancy-preview-card__short-description'}).text
-                company = 'No name'
-                company_block = div.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
-                if company_block and company_block.a:
-                    company = company_block.a.text.strip()
+    if url:
+        response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if response.status_code == 200:
+            soup = Bs(response.content, 'html.parser')
+            main_div = soup.find('div', class_='infinity-scroll')
+            if main_div:
+                div_list = main_div.find_all('div', attrs={'class': 'vacancy-preview-card__top'})
+                for div in div_list:
+                    title = div.find('h3')
+                    href = title.a['href']
+                    content = div.find('div', attrs={'class': 'vacancy-preview-card__short-description'}).text
+                    company = 'No name'
+                    company_block = div.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
+                    if company_block and company_block.a:
+                        company = company_block.a.text.strip()
 
-                jobs.append({
-                    'title': title.text,
-                    'url': domain + href,
-                    'description': content,
-                    'company': company,
+                    jobs.append({
+                        'title': title.text,
+                        'url': domain + href,
+                        'description': content,
+                        'company': company,
+                        'city_id': city,
+                        'language_id': language,
+                    })
+            else:
+                errors.append({
+                    'url': url,
+                    'title': 'Div does not exist',
                 })
         else:
             errors.append({
                 'url': url,
-                'title': 'Div does not exist',
+                'title': 'Page do not response',
             })
-    else:
-        errors.append({
-            'url': url,
-            'title': 'Page do not response',
-        })
 
-    return jobs, errors
+        return jobs, errors
 
 
-def gorodrabot(url):
+def gorodrabot(url, city=None, language=None):
     jobs = []
     errors = []
     # domain = 'https://moskva.gorodrabot.ru/'
-    response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if response.status_code == 200:
-        soup = Bs(response.content, 'html.parser')
-        main_div = soup.find('div', class_='result-list')
-        if main_div:
-            div_list = main_div.find_all('div', class_ = 'snippet__inner')
-            for div in div_list:
-                title = div.find('h2')
-                href = title.a['href']
-                content = div.find('div', attrs={'class': 'snippet__desc'}).text
-                company = div.find('span', attrs={'class': 'snippet__meta-value'}).text
+    if url:
+        response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if response.status_code == 200:
+            soup = Bs(response.content, 'html.parser')
+            main_div = soup.find('div', class_='result-list')
+            if main_div:
+                div_list = main_div.find_all('div', class_ = 'snippet__inner')
+                for div in div_list:
+                    title = div.find('h2')
+                    href = title.a['href']
+                    content = div.find('div', attrs={'class': 'snippet__desc'}).text
+                    company = div.find('span', attrs={'class': 'snippet__meta-value'}).text
 
-                jobs.append({
-                    'title': title.text,
-                    'url': href,
-                    'description': content,
-                    'company': company,
+                    jobs.append({
+                        'title': title.text,
+                        'url': href,
+                        'description': content,
+                        'company': company,
+                        'city_id': city,
+                        'language_id': language,
+                    })
+            else:
+                errors.append({
+                    'url': url,
+                    'title': 'Div does not exist',
                 })
         else:
             errors.append({
                 'url': url,
-                'title': 'Div does not exist',
+                'title': 'Page do not response',
             })
-    else:
-        errors.append({
-            'url': url,
-            'title': 'Page do not response',
-        })
 
-    return jobs, errors
+        return jobs, errors
 
 
-def superjob(url):
+def superjob(url, city=None, language=None):
     jobs = []
     errors = []
     # domain = 'https://russia.superjob.ru/'
-    response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if response.status_code == 200:
-        soup = Bs(response.content, 'html.parser')
-        main_div = soup.find('div', class_='MokF1')
-        if main_div:
-            div_list = main_div.find_all('div', class_ = 'qjjga')
-            for div in div_list:
-                title = div.find('div', attrs={'class': 'T8ZHO'})
-                href = title.a['href']
+    if url:
+        response = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if response.status_code == 200:
+            soup = Bs(response.content, 'html.parser')
+            main_div = soup.find('div', class_='MokF1')
+            if main_div:
+                div_list = main_div.find_all('div', class_ = 'qjjga')
+                for div in div_list:
+                    title = div.find('div', attrs={'class': 'T8ZHO'})
+                    href = title.a['href']
 
-                content_tag = div.find('span', class_='mOvi3 _2BmV8 OAzFF J1J_3 yqr63')
-                content = content_tag.text.strip() if content_tag else "Нет описания"
+                    content_tag = div.find('span', class_='mOvi3 _2BmV8 OAzFF J1J_3 yqr63')
+                    content = content_tag.text.strip() if content_tag else "Нет описания"
 
-                company_tag = div.find('span', class_='f-test-text-vacancy-item-company-name')
-                company_name = company_tag.a.text.strip() if company_tag and company_tag.a else "Нет компании"
+                    company_tag = div.find('span', class_='f-test-text-vacancy-item-company-name')
+                    company_name = company_tag.a.text.strip() if company_tag and company_tag.a else "Нет компании"
 
-                jobs.append({
-                    'title': title.text,
-                    'url': href,
-                    'description': content,
-                    'company': company_name,
+                    jobs.append({
+                        'title': title.text,
+                        'url': href,
+                        'description': content,
+                        'company': company_name,
+                        'city_id': city,
+                        'language_id': language,
+                    })
+            else:
+                errors.append({
+                    'url': url,
+                    'title': 'Div does not exist',
                 })
         else:
             errors.append({
                 'url': url,
-                'title': 'Div does not exist',
+                'title': 'Page do not response',
             })
-    else:
-        errors.append({
-            'url': url,
-            'title': 'Page do not response',
-        })
 
-    return jobs, errors
+        return jobs, errors
 
 
 # if __name__ == '__main__':
