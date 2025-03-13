df175f5ec7d08faf5971f9612ea035801d50f4c4
MIKHAIL PANTELEEV <94679102+ZeRcooI@users.noreply.github.com>
2025-03-14 00:47:32 +0300

    подготовлена рассылка ошибок для админа

--- src/send_emails.py
+++ src/send_emails.py
@@ -1,5 +1,6 @@
 import os, sys
 import django
+import datetime
 from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth import get_user_model
 
@@ -8,11 +9,16 @@ sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
-from scraping.models import Vacancy
+
+from scraping.models import Vacancy, Error
 from scraping_service.settings import EMAIL_HOST_USER
 
-subject = "Рассылка вакансий"
-text_content = "Рассылка вакансий"
+ADMIN_USER = EMAIL_HOST_USER
+
+today = datetime.date.today()
+
+subject = f"Рассылка вакансий за {today}"
+text_content = f"Рассылка вакансий за {today}"
 from_email = EMAIL_HOST_USER
 
 empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет. </h2>'
@@ -28,7 +34,7 @@ if users_dct:
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values()[:10]
+    qs = Vacancy.objects.filter(**params, timestamp=today).values()[:10]
     vacancies = {}
     for i in qs:
         vacancies.setdefault((i['city_id'], i['language_id']), [])
@@ -37,7 +43,7 @@ if users_dct:
         rows = vacancies.get(keys, [])
         html = ''
         for row in rows:
-            html += f'<h4><a href="{row["url"]}" target="_blank">{row["title"]}</a></h4>'
+            html += f'<h4><a href="{row["url"]}">{row["title"]}</a></h4>'
             html += f'<p>{row["description"]}</p>'
             html += f'<p>{row["company"]}</p><br><hr>'
         _html = html if html else empty
@@ -46,3 +52,17 @@ if users_dct:
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
             msg.attach_alternative(_html, "text/html")
             msg.send()
+
+qs = Error.objects.filter(timestamp=today)
+if qs.exists():
+    error = qs.first()
+    data = error.data
+    _html = ''
+    for i in data:
+        _html += f'<p><a href="{i["url"]}">Error: {i["title"]}</a></p>'
+    subject = ""
+    text_content = ""
+    to = ADMIN_USER
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(_html, "text/html")
+    msg.send()
